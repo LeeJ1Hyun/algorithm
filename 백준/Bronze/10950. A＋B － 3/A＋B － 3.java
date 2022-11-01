9c6a720e1cd026c72cae6424850a68cfcad5abf8
이지현 JiHyunLee <100523786+LeeJ1Hyun@users.noreply.github.com>
2022-11-01 23:08:22 +0900

    [Bronze V] Title: A+B - 3, Time: 128 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+ 
+		for (int i = 0; i < t; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+			sb.append('\n');
+		}
+ 
+		System.out.println(sb);
+    }
+}
