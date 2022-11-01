3a064d50a0ba12111c23679c14e69159c93e24bd
이지현 JiHyunLee <100523786+LeeJ1Hyun@users.noreply.github.com>
2022-11-01 17:04:38 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String surprise = "??!";
+		String nickname = sc.next();
+        
+		System.out.println(nickname + surprise);
+    }
+}
