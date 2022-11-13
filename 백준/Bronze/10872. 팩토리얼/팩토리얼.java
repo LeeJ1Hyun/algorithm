9ce7d6752261774ddf5df79f141052699b43f69d
이지현 JiHyunLee <100523786+LeeJ1Hyun@users.noreply.github.com>
2022-11-13 19:15:36 +0900

    [Bronze V] Title: 팩토리얼, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum = factorial(num);
+        
+        System.out.println(sum);
+    }
+    
+    public static int factorial(int num) {
+        if (num <= 1) return 1;
+        return num * factorial(num - 1);
+    }
+}
