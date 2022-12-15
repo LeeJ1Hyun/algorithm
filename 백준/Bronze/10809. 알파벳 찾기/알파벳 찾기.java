6795eb26d556c3d491b28cb6b232170b46ffcabe
이지현 JiHyunLee <100523786+LeeJ1Hyun@users.noreply.github.com>
2022-12-15 21:22:14 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 144 ms, Memory: 15860 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String S = br.readLine();
+        
+        for(char c = 'a'; c <= 'z'; c++) {
+            sb.append(S.indexOf(c) + " ");
+        }
+        System.out.println(sb);
+    }
+}
